9eaf890093d732dc63e0addb3029aa41d20fd685
SaiSaingWan <6731503084@lamduan.mfu.ac.th>
2025-09-01 09:04:03 +0000

    6731503084

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -1,6 +1,45 @@
 package th.mfu.boot;
 
-public class User {
+public class User
+{
+    private String username;
+    private String displayname;
+    private String email;
+
+    public User()
+    {
+        //constructor
+    }
+     public User(String username, String displayname, String email) {
+        this.username = username;
+        this.displayname = displayname;
+        this.email = email;
+    }
+
+    //getter and setter
+     public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     
 }
